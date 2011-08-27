e8161701b074b20a74e12a7333fb1d41586ad537
Hyunwoo Cho <hc27oclock@gmail.com>
2011-08-28 00:00:58 +0900

    Chaper 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# encoding: UTF-8
+
 module ApplicationHelper
+  def title
+    base_title = "샘플 앱은 진리입니다."
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
