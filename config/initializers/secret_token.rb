2e7d7e542979e87cc5483c82a134146bb5654364
Hyunwoo Cho <hc27oclock@gmail.com>
2011-08-27 23:03:56 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongosa::Application.config.secret_token = '0266064ec32a41d7f871ba3d00bfbbc658897a8a8e1c31bcf8a6b64af4c33fc1d4a8aac0ed143cd595767f17bf127f3feee9483f3225fc4e26b3f00b2d62e4da'
