4082e5e70c49614e9d349984ad0cb04f50624d63
Hyunwoo Cho <hc27oclock@gmail.com>
2011-08-27 23:46:13 +0900

    exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Mongosa::Application.routes.draw do
   get "pages/home"
-
+  get "pages/help"
   get "pages/contact"
   get "pages/about"
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -47,4 +47,17 @@ describe PagesController do
                         :content => @base_title + " | 사이트 정보")
     end
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content => @base_title + " | 도움말")
+    end
+  end
 end
