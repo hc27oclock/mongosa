0f29d9cdb491a91011c158535067690ce459dad2
Hyunwoo Cho <hc27oclock@gmail.com>
2011-08-27 23:37:53 +0900

    Chapter 3 finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
+# encoding: UTF-8
+
 class PagesController < ApplicationController
   def home
+    @title = "시작 화면"
   end
 
   def contact
+    @title = "개발자에게 연락하기"
   end
 
+  def about
+    @title = "사이트 정보"
+  end
 end
